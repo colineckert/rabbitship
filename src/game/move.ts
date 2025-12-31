6f41a8433e540c4a996dc5d42f2d73b406097923
Colin Eckert <colineckert10@gmail.com>
2025-12-30 21:32:06 -0800

    update move result event type

--- src/game/move.ts
+++ src/game/move.ts
@@ -5,7 +5,8 @@ import {
   type PlayerId,
   type ShipKey,
   ShipLengthMap,
-} from "@/game/types";
+  EVENT_TYPE,
+} from '@/game/types';
 
 function isValidCoord(x: number, y: number): boolean {
   return x >= 0 && x < 10 && y >= 0 && y < 10;
@@ -19,14 +20,14 @@ function resolveShot(
   state: GameState,
   shooter: PlayerId,
   x: number,
-  y: number,
+  y: number
 ): ShotResult {
-  const opponent = shooter === "p1" ? state.p2 : state.p1;
-  const shooterBoard = shooter === "p1" ? state.p1 : state.p2;
+  const opponent = shooter === 'p1' ? state.p2 : state.p1;
+  const shooterBoard = shooter === 'p1' ? state.p1 : state.p2;
 
   const coordKey = `${x},${y}`;
   if (shooterBoard.shots.has(coordKey)) {
-    console.log("Coordinate already shot at.");
+    console.log('Coordinate already shot at.');
     return { hit: false };
   }
 
@@ -35,9 +36,9 @@ function resolveShot(
   const cell = opponent.grid[y][x];
 
   // Miss
-  if (cell === "empty" || cell === "miss" || cell.endsWith("-hit")) {
-    opponent.grid[y][x] = "miss";
-    console.log("Shot result: Miss.");
+  if (cell === 'empty' || cell === 'miss' || cell.endsWith('-hit')) {
+    opponent.grid[y][x] = 'miss';
+    console.log('Shot result: Miss.');
     return { hit: false };
   }
 
@@ -67,24 +68,24 @@ function resolveShot(
 // TODO: make use of function when integrating move handling
 export function handleMove(
   state: GameState,
-  move: MoveEvent,
+  move: MoveEvent
 ): MoveResultEvent | null {
   console.log();
-  console.log("==== Shot Detected ====");
+  console.log('==== Shot Detected ====');
   console.log(
-    `${move.player} fires shot at coordinates [${move.x}, ${move.y}]`,
+    `${move.player} fires shot at coordinates [${move.x}, ${move.y}]`
   );
 
   const isValidMove = isValidCoord(move.x, move.y);
   if (!isValidMove) {
-    console.log("Invalid move: coordinates out of bounds.");
-    throw new Error("Invalid move: coordinates out of bounds.");
+    console.log('Invalid move: coordinates out of bounds.');
+    throw new Error('Invalid move: coordinates out of bounds.');
   }
 
   const shotResult = resolveShot(state, move.player, move.x, move.y);
 
   const moveResult: MoveResultEvent = {
-    type: "move-result",
+    type: EVENT_TYPE.MOVE_RESULT,
     x: move.x,
     y: move.y,
     hit: shotResult.hit,
@@ -92,7 +93,7 @@ export function handleMove(
     shipsSunk: { p1: state.p1.shipsSunk, p2: state.p2.shipsSunk },
     p1Board: state.p1.grid,
     p2Board: state.p2.grid,
-    nextTurn: move.player === "p1" ? "p2" : "p1",
+    nextTurn: move.player === 'p1' ? 'p2' : 'p1',
   };
 
   // TODO: publish move result event to message broker
